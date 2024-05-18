7e4d757e9d236f31737d5a961f082ca656dac0fd
babaygt <yigitingbaba@gmail.com>
2024-05-18 12:48:35 +0200

    Add header and desktop navigation components

---
+++ components/Header.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+
+// components
+import Navigation from './Navigation'
+
+const Header = () => {
+	return (
+		<header className='py-8 xl:py-12 text-white'>
+			<div className='container mx-auto flex justify-between items-center'>
+				{/* logo */}
+				<Link href='/'>
+					<h1 className='text-4xl font-semibold'>
+						Safe <span className='text-accent'>Bike</span>
+					</h1>
+				</Link>
+				{/* desktop nav */}
+				<div className='hidden xl:flex items-center gap-8'>
+					<Navigation />
+				</div>
+			</div>
+		</header>
+	)
+}
+export default Header
